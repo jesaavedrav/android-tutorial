951f7cf24f76b1de1b5909ae9f56ca34f7c3fc3c
Jorge Saavedra <jsaavedra@montran.com>
2020-11-02 21:14:17 -0500

    Practica 3

--- app/src/main/java/com/jsaavedra/practica1/SecondActivity.java
+++ app/src/main/java/com/jsaavedra/practica1/SecondActivity.java
@@ -5,18 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.Objects;
 
-public class SecondActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button killActivityStack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        killActivityStack = findViewById(R.id.killActivityStack);
+        killActivityStack.setOnClickListener(this);
+
         Intent secondActivityIntent = getIntent();
         Log.d("String Data:", Objects.requireNonNull(secondActivityIntent.getStringExtra("stringName")));
         Log.d("Integer Data:", String.valueOf(secondActivityIntent.getIntExtra("integerName", 0)));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.killActivityStack:
+                Intent killAllIntent = new Intent(this, MainActivity.class);
+                killAllIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(killAllIntent);
+                //finishAffinity(); this kills all activities including this one
+        }
     }
 }
