7abca8667e999b3c8beb60efe21d5c83e24f65ee
Jorge Saavedra <jsaavedra@montran.com>
2020-11-02 20:54:41 -0500

    Practica 2

--- app/src/main/java/com/jsaavedra/practica1/MainActivity.java
+++ app/src/main/java/com/jsaavedra/practica1/MainActivity.java
@@ -2,11 +2,13 @@ package com.jsaavedra.practica1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button nextActivityButton;
 
@@ -16,14 +18,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         nextActivityButton = findViewById(R.id.nextActivityButton);
+        // 2nd way to listen a Button
+        nextActivityButton.setOnClickListener(this);
 
         // 1st way to listen to Button
+//        nextActivityButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.d("Click", "Next activity clicked");
+//            }
+//        });
 
-        nextActivityButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-            }
-        });
+    }
+
+    // 2nd way to listen a Button
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.nextActivityButton:
+                Log.d("Click", "Next activity clicked from interface");
+                Intent nextActivityIntent = new Intent(this, SecondActivity.class);
+                nextActivityIntent.putExtra("stringName", "this is a String");
+                nextActivityIntent.putExtra("integerName", 654);
+                startActivity(nextActivityIntent);
+                break;
+        }
     }
 }
--- app/src/main/java/com/jsaavedra/practica1/SecondActivity.java
+++ app/src/main/java/com/jsaavedra/practica1/SecondActivity.java
@@ -2,7 +2,11 @@ package com.jsaavedra.practica1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Objects;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +14,9 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Intent secondActivityIntent = getIntent();
+        Log.d("String Data:", Objects.requireNonNull(secondActivityIntent.getStringExtra("stringName")));
+        Log.d("Integer Data:", String.valueOf(secondActivityIntent.getIntExtra("integerName", 0)));
     }
 }
